822ae28c89fd927b3246a289011a4c2975222a0f
Leo Kirasic <kirasicleo@gmail.com>
2022-02-23 19:12:02 +0100

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Nav from './components/Nav';
+import Shop from './components/Shop';
 
 function App() {
-  return <div className="">rare store</div>;
+  return (
+    <Router>
+      <div className="flex flex-col flex-wrap content-center">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home() {
+  return <div>Rare Store</div>;
+}
+
+export default Home;
