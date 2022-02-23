5858ba6a68e003145a1f83737c4e325a10e93796
Leo Kirasic <kirasicleo@gmail.com>
2022-02-23 19:33:11 +0100

    add uniqid to items

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import Card from './Card';
+import uniqid from 'uniqid';
 function Shop() {
   const items = [
-    { title: 'Black Crewneck', img: require('../assets/black-crewneck.png') },
-    { title: 'Black Hoodie', img: require('../assets/black-hoodie.png') },
-    { title: 'Black Shirt', img: require('../assets/black-shirt.png') },
+    {
+      id: uniqid(),
+      title: 'Black Crewneck',
+      img: require('../assets/black-crewneck.png'),
+    },
+    {
+      id: uniqid(),
+      title: 'Black Hoodie',
+      img: require('../assets/black-hoodie.png'),
+    },
+    {
+      id: uniqid(),
+      title: 'Black Shirt',
+      img: require('../assets/black-shirt.png'),
+    },
   ];
   function renderItems() {
     const listItems = items.map((item) => (
-      <Card key={item.title} title={item.title} img={item.img} />
+      <Card id={item.id} key={item.title} title={item.title} img={item.img} />
     ));
     return listItems;
   }
