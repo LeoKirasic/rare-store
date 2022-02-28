be2414b65b7ef9a72c84d98ce56b1d3ee7dbea47
Leo Kirasic <kirasicleo@gmail.com>
2022-02-28 16:33:34 +0100

    Add Cart to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,6 +10,9 @@ function Nav() {
         <Link to="/shop">
           <li>Shop</li>
         </Link>
+        <Link to="/cart">
+          <li>Cart</li>
+        </Link>
       </ul>
     </nav>
   );
