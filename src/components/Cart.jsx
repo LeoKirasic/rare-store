3a818b23f382de7d35dea683540a0b1cdab7e7ca
Leo Kirasic <kirasicleo@gmail.com>
2022-02-28 19:37:17 +0100

    Add display of sum of all items

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CartCard from './CartCard';
 import propTypes from 'prop-types';
 function Cart(props) {
+  console.log('cart props', props);
   function renderItems() {
     const listItems = props.items.map((item) => (
       <CartCard
@@ -10,16 +11,33 @@ function Cart(props) {
         title={item.title}
         img={item.img}
         amount={item.amount}
+        amountChangeHandler={props.amountChangeHandler}
+        deleteItem={props.deleteItem}
+        price={item.price}
       />
     ));
     return listItems;
   }
-
-  return <div className="flex">{renderItems()}</div>;
+  function getSumOfItems() {
+    let total = 0;
+    for (let i = 0; i < props.items.length; i++) {
+      total += props.items[i].price * props.items[i].amount;
+    }
+    return total;
+  }
+  return (
+    <div>
+      <div className="flex">{renderItems()}</div>
+      <div>Total price: {getSumOfItems()}</div>
+    </div>
+  );
 }
 
 Cart.propTypes = {
   items: propTypes.array,
+  amountChangeHandler: propTypes.func,
+  deleteItem: propTypes.func,
+  price: propTypes.number,
 };
 
 export default Cart;
