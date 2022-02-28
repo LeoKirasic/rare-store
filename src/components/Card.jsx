4e404994211d75f3051d061a64acf3b78176a249
Leo Kirasic <kirasicleo@gmail.com>
2022-02-28 19:36:07 +0100

    Add addToCart Functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import propTypes from 'prop-types';
 function Card(props) {
-  return <img className=" w-32" src={props.img} alt="" />;
+  return (
+    <div className="">
+      <img className="w-24" src={props.img} alt="" />
+      <div>${props.price}</div>
+      <button onClick={() => props.addToCartHandler(props)}>Add to Cart</button>
+    </div>
+  );
 }
 
 export default Card;
 
 Card.propTypes = {
   img: propTypes.string,
+  addToCartHandler: propTypes.func,
+  price: propTypes.number,
 };
