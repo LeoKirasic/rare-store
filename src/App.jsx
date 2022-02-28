3d34cd47d542fe61f0782764e17a8b0faaf52d76
Leo Kirasic <kirasicleo@gmail.com>
2022-02-28 16:33:11 +0100

    Move items from Shop component to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,50 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Nav from './components/Nav';
 import Shop from './components/Shop';
-
+import Cart from './components/Cart';
+import uniqid from 'uniqid';
+import { useState } from 'react';
 function App() {
+  const [items, setItems] = useState([
+    {
+      id: uniqid(),
+      title: 'Black Crewneck',
+      img: require('./assets/black-crewneck.png'),
+      amount: 1,
+    },
+    {
+      id: uniqid(),
+      title: 'Black Hoodie',
+      img: require('./assets/black-hoodie.png'),
+      amount: 1,
+    },
+    {
+      id: uniqid(),
+      title: 'Black Shirt',
+      img: require('./assets/black-shirt.png'),
+      amount: 1,
+    },
+  ]);
+  const [cartItems, setCartItems] = useState([]);
+  function addToCartHandler(item) {
+    const found = cartItems.findIndex(
+      (element) => element.title === item.title
+    );
+    if (found === -1) {
+      const updatedItems = [...cartItems, item];
+
+      setCartItems(updatedItems);
+    } else {
+      const updatedItems = JSON.parse(JSON.stringify(cartItems));
+      console.log('updated items', updatedItems);
+      updatedItems[found].amount++;
+
+      setCartItems(updatedItems);
+    }
+
+    console.log(cartItems);
+  }
+
   return (
     <Router>
       <div className="flex flex-col flex-wrap content-center">
@@ -12,7 +54,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
+          <Route
+            path="/shop"
+            element={<Shop items={items} addToCartHandler={addToCartHandler} />}
+          />
+          <Route path="/cart" element={<Cart items={cartItems} />} />
         </Routes>
       </div>
     </Router>
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,31 +1,27 @@
 import React from 'react';
 import Card from './Card';
-import uniqid from 'uniqid';
-function Shop() {
-  const items = [
-    {
-      id: uniqid(),
-      title: 'Black Crewneck',
-      img: require('../assets/black-crewneck.png'),
-    },
-    {
-      id: uniqid(),
-      title: 'Black Hoodie',
-      img: require('../assets/black-hoodie.png'),
-    },
-    {
-      id: uniqid(),
-      title: 'Black Shirt',
-      img: require('../assets/black-shirt.png'),
-    },
-  ];
+import propTypes from 'prop-types';
+function Shop(props) {
   function renderItems() {
-    const listItems = items.map((item) => (
-      <Card id={item.id} key={item.title} title={item.title} img={item.img} />
+    const listItems = props.items.map((item) => (
+      <Card
+        id={item.id}
+        key={item.title}
+        title={item.title}
+        img={item.img}
+        addToCartHandler={props.addToCartHandler}
+        amount={item.amount}
+      />
     ));
     return listItems;
   }
   return <div className="flex">{renderItems()}</div>;
 }
+
+Shop.propTypes = {
+  items: propTypes.array,
+  addToCartHandler: propTypes.func,
+  amount: propTypes.number,
+};
 
 export default Shop;
